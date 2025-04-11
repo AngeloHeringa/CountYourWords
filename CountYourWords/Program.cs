f9609e3be9b3ddff181774231cc1d8f1f0fd628c
Angelo Heringa <48793053+AngeloHeringa@users.noreply.github.com>
2025-04-11 12:44:13 +0200

    Initial setup

---
+++ CountYourWords/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CountYourWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");        
+        }
+    }
+}
